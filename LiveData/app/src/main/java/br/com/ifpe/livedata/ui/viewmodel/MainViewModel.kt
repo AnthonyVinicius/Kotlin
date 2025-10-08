4af3344d10590ee408529222e89e3dbec3942e64
Anthony Vinicius <91100881+AnthonyVinicius@users.noreply.github.com>
2025-10-08 20:48:44 -0300

    Update

--- LiveData/app/src/main/java/br/com/ifpe/livedata/ui/viewmodel/MainViewModel.kt
+++ LiveData/app/src/main/java/br/com/ifpe/livedata/ui/viewmodel/MainViewModel.kt
@@ -3,13 +3,18 @@ package br.com.ifpe.livedata.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.ifpe.livedata.ui.UiStateUser
 import br.com.ifpe.livedata.ui.model.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel : ViewModel(){
 
     private val _usuario = MutableLiveData<User>()
     val usuario: LiveData<User> get() = _usuario
 
+    private val _uiStateUser =  MutableStateFlow<UiStateUser>(UiStateUser.Loading)
+    val uiStateUser: StateFlow<UiStateUser> = _uiStateUser
     fun carregarUsuario(){
         _usuario.value = User("Anthony", 21)
     }
